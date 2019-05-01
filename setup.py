b91630e86c730be71f449c9d2a0ce26e20618d45
Diwakar Reddy Malla <mdiwakar50@gmail.com>
2019-04-30 23:58:24 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image classification model for detecting landmarks',
+    author='Diwakar',
+    license='MIT',
+)
